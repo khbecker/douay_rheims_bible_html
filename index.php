4ca3f8c6963e947ad313ac00e9347d752ab3783a
Kevin Becker <khbecker@gmail.com>
2012-05-26 08:54:22 -0400

    Reduced extra line breaks in HEAD. Switched to absolute URLs in frames since Internet Explorer didn't like relative paths combined with mod_rewrite. Need to investigate how to make these relative in the future.

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta name="generator" content=
-"HTML Tidy for Linux/x86 (vers 25 March 2009), see www.w3.org" />
+<meta name="generator" content="HTML Tidy for Linux/x86 (vers 25 March 2009), see www.w3.org" />
 <title>The 1899 Douay-Rheims Catholic Bible</title>
 <meta name="language" content="En" />
 <meta name="classification" content="global" />
 <meta name="keywords" content=
 "free bible downloads, bible, on-line, free bible software, free software,free downloads, bible downloads,bible software,html bible,internet bible,christian resources,free,onlinebible,Douay-Rheims, Catholic Bible,catholic" />
-<meta name="description" content=
-"The 1899 Douay-Rheims Catholic Bible" />
-<base href="http://www.saintbenedicts.com/bible/"/>
+<meta name="description" content="The 1899 Douay-Rheims Catholic Bible" />
 <link rel="shortcut icon" href="http://www.saintbenedicts.com/bible/bible.ico" />
 <script type="text/javascript">
 
@@ -24,7 +20,7 @@ function load_page()
     
     if( book.length > 0 )
     {
-        page = book + ".htm";
+        page = "http://www.saintbenedicts.com/bible/" + book + ".htm";
         
         if( chapter.length > 0 )
         {
@@ -46,7 +42,7 @@ function load_page()
 </script>
 </head>
 <frameset cols="202,*" onload="load_page()">
-<frame src="menu.htm" />
+<frame src="http://www.saintbenedicts.com/bible/menu.htm" />
 <frame name="sbpages" src="" id="sbpages" />
 </frameset>
 </html>
